686eeb948d9b6d1c05b0edf40e9aa7165dec1795
Sweta Singh <51352544+Sweta1Singh@users.noreply.github.com>
2022-10-02 23:46:31 +0530

    Eliminating duplicates from array.

---
+++ JAVASCRIPT/dupe_elimination.js
@@ -0,0 +1,5 @@
+//cerner_2^5_2022
+//Eliminating duplicates from array
+//compile on https://www.programiz.com/javascript/online-compiler/
+const eleminatedupes = (arr) => [...new Set(arr)]
+console.log(eleminatedupes([21,99,43,43,12,21,81,99]))
