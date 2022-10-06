d21b5a2960755a7a56826889f4988d96bbedc95c
Sweta Singh <51352544+Sweta1Singh@users.noreply.github.com>
2022-10-06 23:52:32 +0530

    TypeScript program to reverse a string.

---
+++ TYPESCRIPT/Reverse_String.ts
@@ -0,0 +1,8 @@
+//cerner_2^5_2022
+//reverse a string
+//compile on https://www.mycompiler.io/new/typescript
+let toBeReversed: string = `.margroP tpircSepyT siht gnisu desrever gnieb si ecnetnes sihT`;
+let charArray: string[] = toBeReversed.split('');
+let reverseArray: string[] = charArray.reverse();
+let reversedArray: string = reverseArray.join('');
+console.log(reversedArray);
