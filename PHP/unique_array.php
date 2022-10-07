a45385b25be6182922b279ebe83fa0e1ce66e82b
Sweta Singh <51352544+Sweta1Singh@users.noreply.github.com>
2022-10-07 23:45:51 +0530

    unique array program

---
+++ PHP/unique_array.php
@@ -0,0 +1,12 @@
+//cerner_2^5_2022
+//Unique array
+//compile on https://www.w3schools.com/php/phptryit.asp?filename=tryphp_compiler
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$arr = array("A","E","I","I","O","O","U","U");
+print_r(array_unique($arr));
+?>
+</body>
+</html>
